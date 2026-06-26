4c1cde7e119305415ac5a18d4bd63257149dc0a8
jjrock98 <clarosjavier1998@gmail.com>
2026-06-26 03:45:58 -0300

    Agregar verificación de Google Search Console vía meta tag

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const appUrl = process.env.NEXT_PUBLIC_APP_URL ?? 'https://localhost:3000';
+  const appUrl = process.env.NEXT_PUBLIC_APP_URL ?? 'https://mc-importados.xyz';
 
   return {
     rules: [
